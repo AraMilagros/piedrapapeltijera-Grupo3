d77e2a1dbc89bf83adf5ed198bfb2ed54689f547
Lucas Martinez LLosco <linkgz55@gmail.com>
2022-09-25 09:46:16 -0300

    Se corrigieron errores en el componente juego y home -Lucas Martinez

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import '../assets/css/Home.css';
-
+import Header from '../components/Header';
 export default function Home() {
   return (
     <>
+    <Header/>
         <div className="portada">
           <h1>PIEDRA PAPEL O TIJERAS</h1>
           <img src="./img/PPT.gif" alt="" />
           <div className="contenedor-btn">
-            <a class="btn btn-danger" href="/jugar">¡A jugar!</a>
-            <a class="btn btn-info" href="/info">¿Quiénes somos?</a>
+            <a className="btn btn-danger" href="/jugar">¡A jugar!</a>
+            <a className="btn btn-info" href="/info">¿Quiénes somos?</a>
           </div>
         </div>
     </>
